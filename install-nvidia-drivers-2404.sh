8c38e875913fa3f2e3ad1e209d2c26837165fde7
earguedast <este.at@gmail.com>
2025-10-02 11:37:05 -0600

    Update install-nvidia-drivers-2404.sh

--- install-nvidia-drivers-2404.sh
+++ install-nvidia-drivers-2404.sh
@@ -33,12 +33,16 @@ sudo apt update -y
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
+echo "Installing the NVIDIA toolkit..."
 sudo apt-get install -y cuda-toolkit-12-9
+
+echo "Installing the NVIDIA Driver..."
 sudo apt-get install -y cuda-drivers-575
 
-#show nvidia gpu information
+# Display the monitor and manage NVIDIA Graphics Processing Unit (GPU) devices on a system.
 nvidia-smi
 
+echo "Installing the NVIDIA fabricmanager..."
 sudo apt install nvidia-fabricmanager-575
 sudo systemctl enable nvidia-fabricmanager.service
 sudo systemctl start nvidia-fabricmanager.service
