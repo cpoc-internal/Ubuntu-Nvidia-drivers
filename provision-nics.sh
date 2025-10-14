308745ce3777b8f2ef1d833d155f55c1fae73b07
earguedast <este.at@gmail.com>
2025-10-14 12:01:55 -0600

    Update provision-nics.sh

--- provision-nics.sh
+++ provision-nics.sh
@@ -6,27 +6,27 @@ echo "provisioning nics ip addresses"
 sudo apt install net-tools -y
 
 sudo ip addr add 192.168.200.2/24 dev ens201f0np0
-ip link set dev ens201f0np0 up
+sudo ip link set dev ens201f0np0 up
 
 sudo ip addr add 192.168.200.3/24 dev ens202f0np0
-ip link set dev ens202f0np0 up
+sudo ip link set dev ens202f0np0 up
 
 sudo ip addr add 192.168.200.4/24 dev ens203f0np0
-ip link set dev ens203f0np0 up
+sudo ip link set dev ens203f0np0 up
 
 sudo ip addr add 192.168.200.5/24 dev ens204f0np0
-ip link set dev ens204f0np0 up
+sudo ip link set dev ens204f0np0 up
 
 sudo ip addr add 192.168.200.6/24 dev ens205f0np0
-ip link set dev ens205f0np0 up
+sudo ip link set dev ens205f0np0 up
 
 sudo ip addr add 192.168.200.7/24 dev ens206f0np0
-ip link set dev ens206f0np0 up
+sudo ip link set dev ens206f0np0 up
 
 sudo ip addr add 192.168.200.8/24 dev ens207f0np0
-ip link set dev ens207f0np0 up
+sudo ip link set dev ens207f0np0 up
 
 sudo ip addr add 192.168.200.9/24 dev ens208f0np0
-ip link set dev ens208f0np0 up
+sudo ip link set dev ens208f0np0 up
 
 ifconfig 
