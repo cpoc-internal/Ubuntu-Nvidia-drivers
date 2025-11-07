559b225dfcb53cc2a4c94a5fef2ff2b13222b874
earguedast <este.at@gmail.com>
2025-11-07 11:11:10 -0600

    update

--- provision-nics-H200-1.sh
+++ provision-nics-H200-1.sh
@@ -6,27 +6,27 @@ echo "provisioning nics ip addresses"
 sudo apt install net-tools -y
 
 sudo ip addr add 192.168.200.11/24 dev ens201f0np0
-sudo ip link set dev ens201f0np0 up
+sudo ip link set dev ens201np0 up
 
 sudo ip addr add 192.168.200.12/24 dev ens202f0np0
-sudo ip link set dev ens202f0np0 up
+sudo ip link set dev ens202np0 up
 
 sudo ip addr add 192.168.200.13/24 dev ens203f0np0
-sudo ip link set dev ens203f0np0 up
+sudo ip link set dev ens203np0 up
 
 sudo ip addr add 192.168.200.14/24 dev ens204f0np0
-sudo ip link set dev ens204f0np0 up
+sudo ip link set dev ens204np0 up
 
 sudo ip addr add 192.168.200.15/24 dev ens205f0np0
-sudo ip link set dev ens205f0np0 up
+sudo ip link set dev ens205np0 up
 
 sudo ip addr add 192.168.200.16/24 dev ens206f0np0
-sudo ip link set dev ens206f0np0 up
+sudo ip link set dev ens206np0 up
 
 sudo ip addr add 192.168.200.17/24 dev ens207f0np0
-sudo ip link set dev ens207f0np0 up
+sudo ip link set dev ens207np0 up
 
 sudo ip addr add 192.168.200.18/24 dev ens208f0np0
-sudo ip link set dev ens208f0np0 up
+sudo ip link set dev ens208np0 up
 
 ifconfig 
