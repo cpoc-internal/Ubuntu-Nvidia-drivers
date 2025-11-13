4ecf4fdbc4bb186ccd9f9e2d360c5712d8c4ec1b
earguedast <este.at@gmail.com>
2025-11-12 21:03:45 -0600

    Update netplan-config-H100.sh

--- netplan-config-H100.sh
+++ netplan-config-H100.sh
@@ -15,9 +15,9 @@ network:
             - 192.168.200.2/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
         ens202f0np0:
@@ -25,31 +25,31 @@ network:
             - 192.168.200.3/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
-                
+
 
         ens203f0np0:
             addresses:
             - 192.168.200.4/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
-                
+
 
         ens204f0np0:
             addresses:
             - 192.168.200.5/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
         ens205f0np0:
@@ -57,42 +57,43 @@ network:
             - 192.168.200.6/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
-        
+
         ens206f0np0:
             addresses:
             - 192.168.200.7/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
-                
+
 
         ens207f0np0:
             addresses:
             - 192.168.200.8/24
             nameservers:
                 addresses:
-                -
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
-                
+
 
         ens208f0np0:
             addresses:
             - 192.168.200.9/24
             nameservers:
                 addresses:
-                -
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
+
               
 
 EOF
