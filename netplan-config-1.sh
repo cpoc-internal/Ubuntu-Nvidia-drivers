3f7b2a6e64cc452c88382a35297628d40c1805e0
earguedast <este.at@gmail.com>
2025-11-12 21:05:52 -0600

    Update netplan-config-1.sh

--- netplan-config-1.sh
+++ netplan-config-1.sh
@@ -12,82 +12,82 @@ network:
     ethernets:
         ens201np0:
             addresses:
-            - 192.168.200.11/18
+            - 192.168.200.2/24
             nameservers:
                 addresses:
-                - 
+                - 192.168.200.1
             routes:
-                - to: default
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
         ens202np0:
             addresses:
-            - 192.168.200.12/24
+            - 192.168.200.3/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
         ens203np0:
             addresses:
-            - 192.168.200.13/24
+            - 192.168.200.4/24
             nameservers:
                 addresses:
-                -
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
         ens204np0:
             addresses:
-            - 192.168.200.14/24
+            - 192.168.200.5/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
         ens205np0:
             addresses:
-            - 192.168.200.15/24
+            - 192.168.200.6/24
             nameservers:
                 addresses:
-                -
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
         
         ens206np0:
             addresses:
-            - 192.168.200.16/24
+            - 192.168.200.7/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
         ens207np0:
             addresses:
-            - 192.168.200.17/24
+            - 192.168.200.8/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
         ens208np0:
             addresses:
-            - 192.168.200.18/24
+            - 192.168.200.9/24
             nameservers:
                 addresses:
-                - 
-             routes:
-                - to: default
+                - 192.168.200.1
+            routes:
+                - to: 192.168.200.0/24
                   via: 192.168.200.1
 
 EOF
